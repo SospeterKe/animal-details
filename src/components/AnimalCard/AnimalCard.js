08872af7e5243ee35f90a985a08d27df3401a5b5
techsavvysos <kedogosospeter36@gmail.com>
2023-04-12 19:21:42 +0100

    added children props

--- src/components/AnimalCard/AnimalCard.js
+++ src/components/AnimalCard/AnimalCard.js
@@ -2,17 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import '../AnimalDetails/AnimalDetails'
 import AnimalDetails from '../AnimalDetails/AnimalDetails';
+import Card from '../card/Card';
 
 export default function AnimalCard({name, size, ...props}) {
     
     return(
-        <div>
+        <Card title='Animal' details={<em><AnimalDetails{...props}/></em>}>
             <h3>{name}</h3>
             <div>{size}kgs</div>
-            <div><AnimalDetails {...props}/></div>
             
             
-        </div>
+        </Card>
     );
 
 }
---
+++ src/components/card/Card.js
@@ -0,0 +1,30 @@
+import React from "react";
+import PropTypes from 'prop-types'
+import './Card.css'
+
+
+export default function Card({children, title, details}){
+
+    return(
+        <div className="Card">
+            <div className="card-details">
+                <h2>{title}</h2>
+                {details}
+            </div>
+            {children}
+        </div>
+    );
+}
+
+Card.propTypes = {
+    children: PropTypes.oneOfType([
+        PropTypes.arrayOf(PropTypes.element),
+        PropTypes.element.isRequired
+    ]),
+    title: PropTypes.string.isRequired,
+    details: PropTypes.element
+}
+
+Card.defaultProps = {
+    details: null
+}
